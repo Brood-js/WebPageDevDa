b732cd5e8f101c253b309fdedd54b6f28e5ce826
JaimeOrdonez <jaime.14.01.00@gmail.com>
2023-10-12 08:21:56 -0500

    Information, Mission y Vision con imagenes Agregados

--- src/pages/AboutUs/AboutUs.jsx
+++ src/pages/AboutUs/AboutUs.jsx
@@ -1,5 +1,13 @@
+import MissionVision from "../Mision&Vision/MissionVision"
+import Information from "../information/Information"
+
 const AboutUs = () => {
-  return <div>AboutUs</div>
+  return (
+    <>
+      <div><Information/></div>
+      <div><MissionVision /></div>
+    </>
+  )
 }
 
 export default AboutUs
